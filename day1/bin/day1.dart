5ce775414fc986113f4556691730d7a2f78cfbfa
MuhammadRahman2 <muhammadrahman2042@gmail.com>
2025-09-23 14:08:47 +0500

    day 1

--- day1/bin/day1.dart
+++ day1/bin/day1.dart
@@ -1,5 +1,18 @@
-import 'package:day1/day1.dart' as day1;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${day1.calculate()}!');
+void main() {
+  // String name = "M Rahman";
+  // int age = 25;
+  // double price = 99.99;
+  // bool isActive = true;
+  // var city = "Peshawar"; // Dart will treat it as String
+  // dynamic anything = 10; // can later become "Hello"
+  // print(
+  //   'My name is $name, I am $age years old.My  city is $city .$price $isActive $anything',
+  // );
+
+  /// output get data from user
+  String? name;
+  name = stdin.readLineSync();
+  print('hello $name');
 }
