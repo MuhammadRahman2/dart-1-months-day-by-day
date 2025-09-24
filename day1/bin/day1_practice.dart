a9ee4268cb00cdb4c33a05c8d4f84682e87aa7c0
MuhammadRahman2 <muhammadrahman2042@gmail.com>
2025-09-24 11:34:17 +0500

    day1 practice

---
+++ day1/bin/day1_practice.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  // age Calculate
+  // int birthYear = 2000;
+  // int currentYear = 2025;
+  // int age = currentYear - birthYear;
+  // print('You\'r $age years old');
+
+  // user Greeting
+  print("Enter your name");
+  String? name = stdin.readLineSync();
+  print('Welcome $name');
+}
