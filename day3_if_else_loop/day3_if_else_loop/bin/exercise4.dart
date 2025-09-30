b02bffba63993e85801f9d10bb4ffa3dae110f53
MuhammadRahman2 <muhammadrahman2042@gmail.com>
2025-09-30 10:43:40 +0500

    day3 complet qestion

--- day3_if_else_loop/day3_if_else_loop/bin/exercise4.dart
+++ day3_if_else_loop/day3_if_else_loop/bin/exercise4.dart
@@ -5,8 +5,13 @@ void main() {
   print('Enter a number');
   int table = 0;
   String? number = stdin.readLineSync();
-  for (int i = 1; i <= 10; i++) {
-    table  = int.parse(number!) *  i;
-    print("$number x $i = $table");
+  int num = int.parse(number!);
+  if (num <= 10) {
+    for (int i = 1; i <= 10; i++) {
+      table = num * i;
+      print("$number x $i = $table");
+    }
+  } else {
+    print('Number is greater then 10');
   }
 }
